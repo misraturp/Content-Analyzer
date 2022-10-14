6eac6d06bc74a5dabbc997156d35ce8cd29384d7
misraturp <misraturp@gmail.com>
2022-10-15 01:00:20 +0200

    utils update

--- configure.py
+++ configure.py
@@ -1,2 +1,2 @@
-auth_key="d2991c00b40f435690625fff4e0ed57d"
+auth_key="your api key"
 # get one from https://www.assemblyai.com/?utm_source=youtube&utm_medium=referral&utm_campaign=channel_assemblyai
